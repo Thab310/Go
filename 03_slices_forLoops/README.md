fc7380256863621338355e23e3b7951d5f37a043
Thabelo Ramabulana <th9b310@gmail.com>
2024-05-01 14:51:54 +0200

    Added slices and for loops

---
+++ 03_slices_forLoops/README.md
@@ -0,0 +1,3 @@
+*__Array__* = An array in Go is a fixed-size sequence of elements of the same type and is immutable
+
+*__Slice__* = is an array that is mutable
---
+++ 03_slices_forLoops/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	car := []string {"bmw","mercedez","toyota"}
+	var boat = []string {"kawasaki","honda", "lamborghini"}
+	fmt.Println(car)
+	fmt.Println(boat)
+
+	vehicles := []string {car[len(car)-1], boat[len(boat)-1], plane()}
+	fmt.Println(vehicles)
+
+	for i, vehicle := range vehicles {
+		fmt.Println(i, vehicle)
+	}
+}
+
+func plane() string {
+	return "gulfstream"
+}
