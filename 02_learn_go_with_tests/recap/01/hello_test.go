5795e696d59482f63ac61a56d722c7fd7204cb4f
Thabelo Ramabulana <th9b310@gmail.com>
2025-02-19 22:44:43 +0000

    🧪 Update tests

--- 02_learn_go_with_tests/recap/01/hello_test.go
+++ 02_learn_go_with_tests/recap/01/hello_test.go
@@ -13,14 +13,71 @@ func TestHello(t *testing.T) {
 }
 */
 
+////////////////////////////////////////////////////////
+/*
 package main
 
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Mike")
-	want := "Here we go again :( Thabelo"
+	got := Hello("Thabelo")
+	want := "Hello Thabelo"
 
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+*/
+////////////////////////////////////////////////////////
+/*
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("Hello people", func(t *testing.T) {
+		got := Hello("Thabelo")
+		want := "Hello Thabelo"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Hello world when string is empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello world"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+*/
+////////////////////////////////////////////////////////
+
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("Hello people", func(t *testing.T) {
+		got := Hello("Thabelo", "Venda")
+		want := "Ndaa Thabelo"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Hello world if string is empty", func(t *testing.T) {
+		got := Hello("", "Venda")
+		want := "Ndaa world"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
