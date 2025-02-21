487d58f1188ccf0f10e14369ff64079b663f70a6
Thabelo Ramabulana <th9b310@gmail.com>
2025-02-21 22:55:14 +0000

    ♻️ Update code

--- 02_learn_go_with_tests/recap/03/repeat.go
+++ 02_learn_go_with_tests/recap/03/repeat.go
@@ -1,12 +1,25 @@
+// package iteration
+
+// func Repeat(c string, n int) string {
+
+// 	var repeated string
+
+// 	for i := 0; i < n; i++ {
+// 		repeated += c
+// 	}
+
+// 	return repeated
+// }
+
 package iteration
 
 import "strings"
 
-func Repeat(c string) string {
+func Repeat(c string, n int) string {
 
 	var repeated strings.Builder
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < n; i++ {
 		repeated.WriteString(c)
 	}
 
